7c675f88a7ddb2aa5ba9345f0fd3ade268aa9608
ahmedm512 <1600155@eng.asu.edu.eg>
2020-03-17 10:00:37 +0200

    Updates

---
+++ precache-manifest.7f5a80db171f7784cba9c6bdcf6429b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe544b0d61511187ea0cb0d8c0067fb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b208e856210884b1990",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "da457841a58b8f9cf6dc",
+    "url": "/static/js/2.e09143e3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e09143e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b208e856210884b1990",
+    "url": "/static/js/main.81628e23.chunk.js"
+  },
+  {
+    "revision": "f39c55543d74d4ebee60",
+    "url": "/static/js/runtime-main.096ae76c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
